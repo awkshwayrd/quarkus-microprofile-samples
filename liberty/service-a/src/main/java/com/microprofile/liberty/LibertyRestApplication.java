e4fe9b0dd47a038a94a8982eae941c96f992791f
awkshwayrd <mathur.aksh@gmail.com>
2020-09-07 21:52:28 +0530

    Bootstrapped OpenLiberty and Quarkus services using MicroProfile bootstrapper

---
+++ liberty/service-a/src/main/java/com/microprofile/liberty/LibertyRestApplication.java
@@ -0,0 +1,11 @@
+package com.microprofile.liberty;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class LibertyRestApplication extends Application {
+}
---
+++ quarkus/service-a/src/main/java/com/microprofile/quarkus/QuarkusRestApplication.java
@@ -0,0 +1,11 @@
+package com.microprofile.quarkus;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class QuarkusRestApplication extends Application {
+}
